372f9b4762a3187ec5d123e5823583432ae15764
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-03-09 20:51:06 -0300

    chore: add repository list component

---
+++ src/components/RepositoryList/components/RepositoryItem/index.jsx
@@ -0,0 +1,18 @@
+function RepositoryItem({ repository: { name, description, url }  }) {
+  return (
+    <li>
+      <strong>{name}</strong>
+      <p>{description}</p>
+
+      {
+        url && (
+          <a href={url} target="_blank">
+            Acessar repositório
+          </a>
+        )
+      }
+    </li>
+  );
+}
+
+export default RepositoryItem;
---
+++ src/components/RepositoryList/index.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+import RepositoryItem from './components/RepositoryItem';
+
+function RepositoryList() {
+  const [repositories, setRepositories] = useState([
+    {
+      id: 'unform',
+      name: 'unform',
+      description: 'Forms in React',
+      url: 'https://github.com/unform/unform',
+    }
+  ]);
+
+  return (
+    <section className="repository-list">
+      <h1>Lista de reopsitórios</h1>
+
+      <ul>
+        {
+          repositories.map(repository => (
+            <RepositoryItem key={repository.id} repository={repository} />)
+          )
+        }
+      </ul>
+    </section>
+  );
+}
+
+export default RepositoryList;
