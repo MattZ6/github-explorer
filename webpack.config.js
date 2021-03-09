c9cbc6cdf453c627ea8ad2c1c38f3d278d3628ed
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-03-09 19:59:24 -0300

    chore: configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: resolve(__dirname, 'src', 'index.jsx'),
   output: {
     path: resolve(__dirname, 'dist'),
@@ -9,6 +11,11 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx']
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: resolve(__dirname, 'public', 'index.html'),
+    })
+  ],
   module: {
     rules: [
       {
